fd40ce0ac13cf7095fe2cdbfdb4fa54eef20843a
clodal <joel.koh@me.com>
2018-02-27 15:52:36 +0800

    update siteconfig

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteTitle: 'Clodal Adventures',
   siteDescription: 'For nerds, go-getters and bored people.',
-  siteUrl: 'https://gcn.netlify.com', // Site domain. Do not inculde a trailing slash!
+  siteUrl: 'https://tender-bose-d63475.netlify.com', // Site domain. Do not inculde a trailing slash!
   pathPrefix: '/' // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
 };
