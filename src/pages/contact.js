fc20bcacb2fff6ada8579f39bdfca1fb3b43b35c
clodal <joel.koh@me.com>
2018-03-05 13:51:57 +0800

    update contact page copy

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,10 +1,9 @@
 import React from 'react'
 import styled from 'styled-components'
 import Helmet from 'react-helmet'
-import Img from 'gatsby-image'
 import config from '../utils/siteConfig'
 import ContactForm from '../components/contactform'
-import { Container } from 'semantic-ui-react';
+import { Container } from 'semantic-ui-react'
 
 
 const Wrapper = styled(Container)`
@@ -45,13 +44,7 @@ const Contact = ({data}) => {
       <Wrapper text>
         <Title>Contact</Title>
         <Text>
-          I am a web developer with more than 5 years of experience in creating web applications and services. I can help you design your business infrastructure and your development team management process. I can help you set your testing strategy and build your application's architecture.
-          Regarding the stack, I am currently working extensively with Javascript/Node.js and React.js to build robust web applications and services. My I/P/F-aaS of preference is AWS.
-          <br /><br />
-          This site should provide you with all the information you need but if you still have questions, please consult my LinkedIn profile, my GitHub profile or contact me directly. I like receiving email from people.
-          I am currently available to hire for a limited time of work per week.
-          <br /><br />
-          Email me to talk about your project or just to say hi.
+          {config.contactDesc}
         </Text>
         <ContactForm/>
       </Wrapper>
--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,9 +1,10 @@
 module.exports = {
   siteTitle: 'Clodal Adventures',
-  siteDescription: 'For nerds, go-getters and bored people.',
+  siteDescription: 'For nerds, hipsters and bored people.',
   siteUrl: 'https://www.clodal.com', // Site domain. Do not include a trailing slash!
   siteBio: 'Hi, my name is Joel',
   siteBioDesc: 'I’m a full-stack web developer & designer based in Singapore.',
+  contactDesc: 'Send a message to talk about your project. I\'d love to hear from you ✌🏻',
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
   githubHandle: 'clodal',
   twitterHandle: '_clodal',
