88d7e2e5b174c146d2e8e738202642502bbc7d44
clodal <joel.koh@me.com>
2018-04-07 20:50:08 +0800

    minor update copy

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -2,7 +2,7 @@ module.exports = {
   siteTitle: 'Clodal Adventures',
   siteDescription: 'For nerds, hipsters and bored people.',
   siteUrl: 'https://www.clodal.com', // Site domain. Do not include a trailing slash!
-  siteBio: 'Hi, Joel here',
+  siteBio: 'Hello, Joel here',
   siteBioDesc: 'I’m a full-stack developer from Singapore',
   contactDesc: 'Send a message to talk about your project. I\'d love to hear from you ✌🏻',
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
